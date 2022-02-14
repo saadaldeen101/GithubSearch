94e8d6a2db5ee5d04f5f23d2d31b5bd0f5d24b0b
Saad <saadaldeen2000@hotmail.com>
2022-02-14 17:18:15 +0300

    UI fixes in item view

--- app/src/main/java/com/codecademy/githup/RepositoriesAdapter.kt
+++ app/src/main/java/com/codecademy/githup/RepositoriesAdapter.kt
@@ -3,7 +3,6 @@ package com.codecademy.githup
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -18,7 +17,7 @@ class RepositoriesAdapter: RecyclerView.Adapter<RepositoriesAdapter.ViewHolder>(
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val avatarImageView: ImageView = itemView.findViewById(R.id.userAvatar)
-        val ropoNameTextView: TextView = itemView.findViewById(R.id.ropoName)
+        val ropoNameTextView: TextView = itemView.findViewById(R.id.repoName)
         val userNameTextView: TextView = itemView.findViewById(R.id.userName)
     }
 
